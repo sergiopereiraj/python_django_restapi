9d5b71e23c800d1491c9706865e016cb1c09f3ea
Sergio Pereira Jorquera <91150936+sergiopereiraj@users.noreply.github.com>
2024-10-22 12:22:41 -0300

    first commit

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'companies', views.CompanyViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
